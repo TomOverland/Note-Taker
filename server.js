82d664e2fd8065d6fb269293750e02446b87ad3b
Tom Overland <tomoverland03@gmail.com>
2020-09-22 12:35:18 -0500

    created GET request for root page

--- server.js
+++ server.js
@@ -9,3 +9,12 @@ const PORT = process.env.PORT || 3000;
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Load index page on GET request
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
